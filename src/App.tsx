677c0a84f44402becf7e269b6ff92e2b0112fdcb
Daniel Olugbenga <danielgebga11@gmail.com>
2026-03-17 12:11:46 +0100

    initialize React app with TypeScript, Vite, and Tailwind CSS setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="">Hello</div>
+      <div className="bg-blue-500 p-4 text-4xl text-white">
+        Tailwind is working
+      </div>
+      <div className="text-red-400">Hello</div>
+    </>
+  );
+}
+
+export default App;
