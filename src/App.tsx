3419c4281e9057e9eaf2ad2dd5a59c565ba53ead
Daniel Olugbenga <danielgebga11@gmail.com>
2026-03-30 09:39:39 +0100

    Setup GSAP and lenis render layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,45 @@
-import './App.css';
+import FeaturesSection from './components/layout/FeaturesSection';
+import Hero from './components/layout/Hero';
+import Navbar from './components/layout/Navbar';
+import useLennis from './utils/useLenis';
+import { gsap } from 'gsap';
+import { useGSAP } from '@gsap/react';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { useEffect } from 'react';
 
 function App() {
+  gsap.registerPlugin(useGSAP, ScrollTrigger);
+
+  // This is a workaround to maintain scroll position on refresh when using GSAP matchmedia. Without this, the scroll position resets to the top on refresh, which can be jarring for users.
+  useEffect(() => {
+    let progress = 0;
+
+    const onRefreshInit = () => {
+      // Save current progress
+      progress = window.scrollY / ScrollTrigger.maxScroll(window);
+    };
+
+    const onRefresh = () => {
+      // Scroll back to saved position
+      window.scrollTo(0, progress * ScrollTrigger.maxScroll(window));
+    };
+
+    ScrollTrigger.addEventListener('refreshInit', onRefreshInit);
+    ScrollTrigger.addEventListener('refresh', onRefresh);
+
+    return () => {
+      ScrollTrigger.removeEventListener('refreshInit', onRefreshInit);
+      ScrollTrigger.removeEventListener('refresh', onRefresh);
+    };
+  }, []);
+
+  useLennis();
+
   return (
     <>
-      <div className="">Hello</div>
-      <div className="bg-blue-500 p-4 text-4xl text-white">
-        Tailwind is working
-      </div>
-      <div className="text-red-400">Hello</div>
+      <Navbar />
+      <Hero />
+      <FeaturesSection />
     </>
   );
 }
