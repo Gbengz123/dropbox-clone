b180b2b62352ffac7be1b81e4323a2c9596eb297
Daniel Olugbenga <danielgebga11@gmail.com>
2026-04-06 15:54:23 +0100

    Add icons and animate

--- src/components/ProductsMenu.tsx
+++ src/components/ProductsMenu.tsx
@@ -1,58 +1,74 @@
+import { motion, type Variants } from 'motion/react';
 interface ProductItem {
   title: string;
   desc: string;
   icon: string;
 }
 
+interface ProductsMenuProps {
+  containerVariant: Variants;
+  itemVariant: Variants;
+}
+
 const items: ProductItem[] = [
   {
     title: 'Dropbox',
     desc: 'Store, share and access files across devices',
-    icon: '/icons/dropbox.svg',
+    icon: 'Dropbox-logo-nav.svg',
   },
   {
     title: 'Dash',
     desc: 'Find, organise and protect company content',
-    icon: '/icons/dash.svg',
+    icon: 'dash-logo-nav.svg',
   },
   {
     title: 'Replay',
     desc: 'Review and approve videos faster',
-    icon: '/icons/replay.svg',
+    icon: 'replay-logo-nav.svg',
   },
   {
     title: 'DocSend',
     desc: 'Send documents securely and track activity',
-    icon: '/icons/docsend.svg',
+    icon: 'DocSend-logo-nav.svg',
   },
   {
     title: 'Sign',
     desc: 'Request and add signatures to documents',
-    icon: '/icons/sign.svg',
+    icon: 'dropbox-sign-logo.svg',
   },
   {
     title: 'Early access',
     desc: 'Preview new product experiences',
-    icon: '/icons/arrow.svg',
+    icon: 'arrow_right.svg',
   },
   {
     title: 'Reclaim.ai',
     desc: 'Schedule habits, tasks and meetings with AI',
-    icon: '/icons/reclaim.svg',
+    icon: 'reclaim-logo-nav.svg',
   },
 ];
 
-export default function ProductsMenu() {
+export default function ProductsMenu({
+  containerVariant,
+  itemVariant,
+}: ProductsMenuProps) {
   return (
-    <div className="grid grid-cols-1 gap-2 md:grid-cols-2 xl:grid-cols-[repeat(2,280px)]">
+    <motion.div
+      className="grid grid-cols-1 gap-2 md:grid-cols-2 xl:grid-cols-[repeat(2,280px)]"
+      variants={containerVariant}
+      initial="closed"
+      animate="open"
+    >
       {items.map((item, index) => (
-        <div
+        <motion.div
           key={index}
           className="group xl:hover:bg-faint-bg flex cursor-pointer items-start gap-3 rounded-xl p-4 hover:bg-stone-200"
+          variants={itemVariant}
+          layout
         >
           {/* Icon */}
-          <div className="bg-standard-bg flex h-10 w-10 items-center justify-center rounded-lg">
-            <img src={item.icon} alt="" className="h-5 w-5" />
+          <div className="flex h-10 w-10 items-center justify-center rounded-lg">
+            <img src={item.icon} alt="" className="h-[32px] w-[32px]" />
           </div>
 
           {/* Text */}
@@ -62,8 +78,8 @@ export default function ProductsMenu() {
             </h4>
             <p className="text-faint-text text-sm">{item.desc}</p>
           </div>
-        </div>
+        </motion.div>
       ))}
-    </div>
+    </motion.div>
   );
 }
